2bb0809ea459ac1ff57c6b208f4d30a88470a66f
AllenOtto <kephamoturi@gmail.com>
2023-08-05 23:23:40 +0300

    Creating rock, paper, scissors console app

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPlayer, inputCPU;
+            int randomInt;
+            Console.Write("Choose between ROCK, PAPER and SCISSORS: ");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+            randomInt = rnd.Next(1, 4);
+
+            switch(randomInt)
+            {
+                case 1: 
+
+            }
+        }
+    }
+}
