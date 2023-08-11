503a0a9ba43bdaaed1ab35da03222dcdb444026c
AllenOtto <kephamoturi@gmail.com>
2023-08-11 22:40:42 +0300

    Added condition to help break out of the game loop at will

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -13,96 +13,122 @@ namespace RockPaperScissors
             // Declare your variables
             string inputPlayer, inputCPU;
             int randomInt;
+            bool playAgain = true;
             int scorePlayer = 0;
             int scoreCPU = 0;
 
-            // Get user choice
-            Console.Write("Choose between ROCK, PAPER and SCISSORS: ");
-            inputPlayer = Console.ReadLine();
-            inputPlayer = inputPlayer.ToUpper();
-
-            // Get computer choice
-            Random rnd = new Random();
-            randomInt = rnd.Next(1, 4);
             
-            while(scorePlayer < 3 && scoreCPU < 3) 
-            { 
-                // Compare choices and display result
-                switch(randomInt)
+            
+            while(playAgain)
+            {
+                while (scorePlayer < 3 && scoreCPU < 3)
                 {
-                    case 1:
-                        inputCPU = "ROCK";
-                        Console.WriteLine("Computer chose 'ROCK'");
-                        if(inputPlayer == "ROCK")
-                        {
-                            Console.WriteLine("You also chose 'ROCK'. It's a DRAW!!\n\n");
-                        }
-                        else if(inputPlayer == "PAPER")
-                        {
-                            Console.WriteLine("You chose 'PAPER'. 'PAPER' beats 'ROCK'. You WIN!!\n\n");
-                            scorePlayer++;
-                        }
-                        else if(inputPlayer == "SCISSORS")
-                        {
-                            Console.WriteLine("You chose 'SCISSORS'. 'ROCK' beats 'SCISSORS'. Computer WINS!!\n\n");
-                            scoreCPU++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error 1: Under computer choice ROCK\n\n");
-                        }
-                        break;
-                    case 2:
-                        inputCPU = "PAPER";
-                        Console.Write("Computer chose 'PAPER'\n\n");
-                        if (inputPlayer == "PAPER")
-                        {
-                            Console.WriteLine("You also chose 'PAPER'. It's a DRAW!!\n\n");
-                        }
-                        else if(inputPlayer == "ROCK")
-                        {
-                            Console.WriteLine("'PAPER' beats 'ROCK'. Computer WINS!!\n\n");
-                            scoreCPU++;
-                        }
-                        else if( inputPlayer == "SCISSORS")
-                        {
-                            Console.WriteLine("'SCISSORS' cut 'PAPER'. You WIN!!\n\n");
-                            scorePlayer++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error 2: Under computer choice: PAPER\n\n");
-                        }
-                        break;
-                    case 3:
-                        inputCPU = "SCISSORS";
-                        Console.WriteLine("Computer Chose 'SCISSORS'\n\n");
-                        if(inputPlayer == "SCISSORS")
-                        {
-                            Console.WriteLine("You also chose 'SCISSORS'. It's a draw!\n\n");
-                        }
-                        else if(inputPlayer == "ROCK")
-                        {
-                            Console.WriteLine("'ROCK' trumples 'SCISSORS'. You WIN!\n\n");
-                            scorePlayer++;
-                        }
-                        else if(inputPlayer == "PAPER")
-                        {
-                            Console.WriteLine("'SCISSORS' cut 'PAPER'. Computer WINS!!\n\n");
-                            scoreCPU++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error 3: Under computer choice SCISSORS\n\n");
-                        }
-                        break;
-                    default:
-                    
-                        break;
+                    // Get user choice
+                    Console.Write("Choose between ROCK, PAPER and SCISSORS: ");
+                    inputPlayer = Console.ReadLine();
+                    inputPlayer = inputPlayer.ToUpper();
+
+                    // Get computer choice
+                    Random rnd = new Random();
+                    randomInt = rnd.Next(1, 4);
+
+                    // Compare choices and display result
+                    switch (randomInt)
+                    {
+                        case 1:
+                            inputCPU = "ROCK";
+                            Console.WriteLine("Computer chose 'ROCK'");
+                            if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("You also chose 'ROCK'. It's a DRAW!!\n\n");
+                            }
+                            else if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("You chose 'PAPER'. 'PAPER' beats 'ROCK'. You WIN!!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer == "SCISSORS")
+                            {
+                                Console.WriteLine("You chose 'SCISSORS'. 'ROCK' beats 'SCISSORS'. Computer WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error 1: Under computer choice ROCK\n\n");
+                            }
+                            break;
+                        case 2:
+                            inputCPU = "PAPER";
+                            Console.Write("Computer chose 'PAPER'\n\n");
+                            if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("You also chose 'PAPER'. It's a DRAW!!\n\n");
+                            }
+                            else if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("'PAPER' beats 'ROCK'. Computer WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            else if (inputPlayer == "SCISSORS")
+                            {
+                                Console.WriteLine("'SCISSORS' cut 'PAPER'. You WIN!!\n\n");
+                                scorePlayer++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error 2: Under computer choice: PAPER\n\n");
+                            }
+                            break;
+                        case 3:
+                            inputCPU = "SCISSORS";
+                            Console.WriteLine("Computer Chose 'SCISSORS'\n\n");
+                            if (inputPlayer == "SCISSORS")
+                            {
+                                Console.WriteLine("You also chose 'SCISSORS'. It's a draw!\n\n");
+                            }
+                            else if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("'ROCK' trumples 'SCISSORS'. You WIN!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("'SCISSORS' cut 'PAPER'. Computer WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error 3: Under computer choice SCISSORS\n\n");
+                            }
+                            break;
+                        default:
+
+                            break;
+                    }
                 }
             }
 
-            Console.ReadLine();
+            if(scorePlayer > 2)
+            {
+                Console.WriteLine("Congrats!! You WIN.");
+            } else if(scoreCPU > 2)
+            {
+                Console.WriteLine("Boohoo!! You Lost.");
+            } else
+            {
+                //
+            }
+
+            Console.Write("Play again? (y/n)");
+            string loop = Console.ReadLine();
+            loop = loop.ToLower();
+
+            if (loop == "y")
+            {
+                playAgain = true;
+            } else if (loop == "n") {
+                playAgain = false;
+            }
         }
     }
 }
